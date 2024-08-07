9f8bcb0b3caf9e7550e8fc95ab1cd0073f02d906
KrithikBhat <Krithikb2021@gmail.com>
2024-08-07 19:22:54 -0400

    Addition and Subtraction - Variable Practice

---
+++ VariableExercise2.java
@@ -0,0 +1,14 @@
+public class VariableExercise2 {
+    public static void main(String[] args){
+        int x = 20;
+        x++;
+
+        int y = 30;
+        y--;
+
+        int z = x + y;
+
+        System.out.println(z);
+
+    };
+};
