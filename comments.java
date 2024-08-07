d45a96bab1b020d43246f30c8b601b128bf12232
KrithikBhat <Krithikb2021@gmail.com>
2024-08-07 19:59:29 -0400

    comments practice

---
+++ comments.java
@@ -0,0 +1,10 @@
+public class comments {
+    public static void main(String[] args){
+        //I would rather be 11 feet tall
+        /*The reason for this would be that 
+        I would rather not be stepped on immediately 
+        or just not be able to do anything ever and instead be able
+        to actually do things and reach differet items which I
+        could not before. */
+    }
+}
