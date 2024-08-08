18d3800606dcaad329b20d156ee17e0d32aa1cbd
KrithikBhat <Krithikb2021@gmail.com>
2024-08-07 20:00:44 -0400

    More organized comments

--- comments.java
+++ comments.java
@@ -3,8 +3,9 @@ public class comments {
         //I would rather be 11 feet tall
         /*The reason for this would be that 
         I would rather not be stepped on immediately 
-        or just not be able to do anything ever and instead be able
-        to actually do things and reach differet items which I
-        could not before. */
+        or just not be able to do anything ever 
+        and instead be able
+        to actually do things and reach different items 
+        which I could not before. */
     }
 }
