2b2cda0eae0d3301015e60d4257c41e7c1825e06
KrithikBhat <Krithikb2021@gmail.com>
2024-08-08 19:41:27 -0400

    Person code practice

---
+++ Day 4/action.java
@@ -0,0 +1,6 @@
+public class action {
+    public static void main(String[] args){
+           age.printAge();
+    }
+ 
+}
---
+++ Day 4/person.java
@@ -0,0 +1,15 @@
+public class person {
+    
+        int age = 16;
+
+        public void printAge()
+{
+    System.out.println("Hello " + age);
+};
+        public void makeOlder() {
+            age++;
+        };
+        public void makeYounger() {
+            age--;
+        };
+    };
