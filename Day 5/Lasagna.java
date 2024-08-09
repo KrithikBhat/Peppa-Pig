545f4e4364a3054bef1a93013e9b8c9dc3b6d2a7
KrithikBhat <Krithikb2021@gmail.com>
2024-08-09 19:27:16 -0400

    Lasagna 2

--- Day 5/Lasagna.java
+++ Day 5/Lasagna.java
@@ -1,6 +1,20 @@
 public class Lasagna{
     int expectedminutes = 40;
+    
    public int expectedMinutesInOven(){
+
     return expectedminutes;
    } 
+
+   public remainingMinutesInOven(int NumMinutes){
+    int leftoverminutes = 40 - NumMinutes
+
+    return leftoverminutes;
+   }
+
+   preperationTimeInMinutes(int NumLayers){
+        int prepminutes = 2*NumLayers;
+
+        return prepminutes;
+   }
 }
