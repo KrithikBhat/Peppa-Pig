cd4e3ebc49c9e96a5e6af6a03e29b275bf10f9c5
KrithikBhat <Krithikb2021@gmail.com>
2024-08-09 19:49:06 -0400

    Lasagna 3

--- Day 5/Lasagna.java
+++ Day 5/Lasagna.java
@@ -5,16 +5,33 @@ public class Lasagna{
 
     return expectedminutes;
    } 
-
-   public remainingMinutesInOven(int NumMinutes){
+   
+   // Part 2
+   private remainingMinutesInOven(int NumMinutes){
     int leftoverminutes = 40 - NumMinutes
 
     return leftoverminutes;
    }
 
-   preperationTimeInMinutes(int NumLayers){
+   public preperationTimeInMinutes(int NumLayers){
         int prepminutes = 2*NumLayers;
 
         return prepminutes;
+        // didn't need to add new variable- could have just subtracted during return
    }
+    
+   //Part 3
+
+   private TotalTimeInMinutes(NumLayers, NumMinutes){
+    return (prepminutes)+(NumMinutes)
+   }
+}
+public class lasagnatest{
+    public static void main(String[] args) {
+        Lasagna randomlasagna = new Lasagna();
+        randomlasagna.expectedMinutesInOven() 
+        randomlasagna.remainingMinutesInOven();
+        randomlasagna.preperationTimeInMinutes();
+        randomlasagna.TotalTimeInMinutes();
+    }
 }
