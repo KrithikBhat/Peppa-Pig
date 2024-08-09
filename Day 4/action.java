3413e120b34e67d64bb998de77ad59fdeeef5c9b
KrithikBhat <Krithikb2021@gmail.com>
2024-08-09 17:56:24 -0400

    action fixed

--- Day 4/action.java
+++ Day 4/action.java
@@ -1,6 +1,14 @@
 public class action {
     public static void main(String[] args){
-           age.printAge();
+           person Krithik = new person();
+           Krithik.printAge();
+           Krithik.makeOlder();
+           Krithik.makeYounger();
+           Krithik.SetAge(10);
+           Krithik.SetAgeAndName(24, "Bill");
+           Krithik.getter();
+           Krithik.Moregetter();
+
     }
  
 }
